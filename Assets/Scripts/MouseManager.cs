ecf0475d0a95fcec7d26496369abd3e58bf93cd2
oalberto96 <ochoapublic@gmail.com>
2020-11-17 21:35:04 -0800

    move player on click event

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MouseManager : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class MouseManager : MonoBehaviour
     public Texture2D target; // Cursor for clickable objects like the world
     public Texture2D doorway; // Cursor for doorways
     public Texture2D combat; // Cursor for combat actions
+
+    public EventVector3 OnClickEnvironment;
 
     // Update is called once per frame
     void Update()
@@ -28,10 +31,21 @@ public class MouseManager : MonoBehaviour
             {
                 Cursor.SetCursor(target, new Vector2(16, 16), CursorMode.Auto);
             }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                OnClickEnvironment.Invoke(hit.point);
+            }
         } else
         {
             Cursor.SetCursor(pointer, Vector2.zero, CursorMode.Auto);
         }
         
     }
+}
+
+[System.Serializable]
+public class EventVector3 : UnityEvent<Vector3>
+{
+
 }
